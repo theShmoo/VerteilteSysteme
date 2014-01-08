5800519c5e449c5f74466a10e167940b6186b129
David Pfahler <david@pfahler.at>
2014-01-08 21:56:49 +0100

    [TEST] RMI Test
    Created a nice rmi test

--- src/main/java/client/ClientCli.java
+++ src/main/java/client/ClientCli.java
@@ -2,7 +2,6 @@ package client;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 import java.security.PublicKey;
 import java.util.List;
 
